458b106c3f7901a22901cc9e55dba23cf0ba5934
Joseph <merrypar731@gmail.com>
2025-01-09 06:50:03 +0630

    2th commit till navbar

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,37 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Home from './component/Home.jsx'
+import Product from './component/Product.jsx'
+import About from './component/About.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element:<Home />
+      },
+      {
+        path: "/product",
+        element:<Product />
+      },
+      {
+        path: "/about",
+        element:<About />
+      }
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
